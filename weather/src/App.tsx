aab084d68a6b124c704f2f627c4a1767dae13913
duongbx288 <duongbx183901@gmail.com>
2023-04-12 16:46:26 +0700

    feat: get currentweather & forecast

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,15 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/search/Search";
 import './assets/App.css';
 import Weather from "./components/weather/Weather";
+import { CURRENT_WEATHER_API, FORECAST_WEATHER_API, WEATHER_KEY } from "./api/WeatherAPI";
+import axios from "axios";
+
+type SearchData {
+  value: string;
+  label: string;
+}
 
 
 function App() {
 
+  const [currentWeather, setCurrentWeather] = useState();
+  const [forecastWeather, setForecastWeather] = useState();
+  const [searchData, setSearchData] = useState<SearchData>({value: '', label: ''});
+
+  useEffect(()=>{
+}, []);
+
+
   const handleOnSearchChange = (searchData: any) => {
     const [latitude, longitude] = searchData.value.split(" ");
     console.log(searchData);
+    const currentWeatherFetch = axios.get(`${CURRENT_WEATHER_API}?lat=${latitude}&lon=${longitude}&appid=${WEATHER_KEY}`).then((res) => {
+      console.log(res);
+    });
+    const forecastWeatherFetch = axios.get(`${FORECAST_WEATHER_API}?lat=${latitude}&lon=${longitude}&appid=${WEATHER_KEY}`).then((res) => {
+      console.log(res);
+    });
 
+    // Promise.all([currentWeatherFetch, forecastWeatherFetch]).then((async (response) => {
+    //   const weatherRes = await response[0];
+    //   const forecastRes = await response[1];
+    //   setCurrentWeather({weatherRes});
+    //   setForecastWeather({forecastRes});
+    // }));
   }
 
 
---
+++ weather/src/api/WeatherAPI.ts
@@ -0,0 +1,4 @@
+
+export const CURRENT_WEATHER_API = 'https://api.openweathermap.org/data/2.5/weather';
+export const FORECAST_WEATHER_API = 'https://api.openweathermap.org/data/2.5/forecast';
+export const WEATHER_KEY = '241e5a600d942cc92e1f3c90233dabc0';
