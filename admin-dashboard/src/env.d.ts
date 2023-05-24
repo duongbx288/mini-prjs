1b8697a9042883d40c11ba3d996d028d755aecbd
duongbx288 <duongbx183901@gmail.com>
2023-05-24 10:53:53 +0700

    feat: replace variable in firebaseConfig with env variable

--- admin-dashboard/src/env.d.ts
+++ admin-dashboard/src/env.d.ts
@@ -1,6 +1,9 @@
+// add .env variable here
+
 interface ImportMetaEnv {
-    readonly VITE_REACT_APP_FIREBASE_KEY: string
-    // more env variables...
+    readonly VITE_FIREBASE_API_KEY: string,
+    readonly VITE_FIREBASE_AUTH_DOMAIN: string,
+    readonly VITE_FIREBASE_PROJECT_ID: string
   }
   
   interface ImportMeta {
--- admin-dashboard/src/firebase.ts
+++ admin-dashboard/src/firebase.ts
@@ -7,9 +7,9 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_mq2sNBf5w-5BqfkBzY-N42BZkN60ql4",
-  authDomain: "web-app-a828c.firebaseapp.com",
-  projectId: "web-app-a828c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY ,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: "web-app-a828c.appspot.com",
   messagingSenderId: "399998617083",
   appId: "1:399998617083:web:3c5a343682e0f867ae375e",
