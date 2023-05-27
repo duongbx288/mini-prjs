5fd1c333e88bb943bfb3d69186bd14ca72045e37
duongbx288 <duongbx183901@gmail.com>
2023-05-27 09:10:39 +0700

    feat: add sidebar

--- admin-dashboard/src/App.tsx
+++ admin-dashboard/src/App.tsx
@@ -4,7 +4,6 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Topbar from "./pages/global/Topbar";
 import { Form, Route, Routes } from "react-router-dom";
-import Sidebar from "./pages/global/Sidebar";
 import Contacts from "./pages/contacts/Contacts";
 import FAQuestions from "./pages/faq/FAQ";
 import Bar from "./pages/bar/Bar";
@@ -14,6 +13,7 @@ import Line from "./pages/line/Line";
 import Team from "./pages/team/Team";
 import Calendar from "./pages/calendar/Calendar";
 import Pie from "./pages/pie/Pie";
+import SideBar from "./pages/global/Sidebar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -23,9 +23,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SideBar />
           <main className="content">
             <Topbar />
-            <Sidebar/>
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
--- admin-dashboard/src/pages/global/Sidebar.tsx
+++ admin-dashboard/src/pages/global/Sidebar.tsx
@@ -1,8 +1,94 @@
+import { Sidebar, Menu, MenuItem, sidebarClasses, menuClasses } from "react-pro-sidebar";
+import { tokens } from "../../assets/theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarOutlinedIcon from "@mui/icons-material/CalendarOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useState } from "react";
 
-const Sidebar = () => {
-    return (
-        <div>Sidebar</div>
-    )
-}
+const SideBar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
 
-export default Sidebar;
+  return (
+    // <Box
+    //   sx={{
+    //     "& .ps-sidebar-inner": {
+    //       background: `${colors.primary[400]} !important`,
+    //     },
+    //     "& .ps-icon-wrapper": {
+    //       backgroundColor: "transparent !important",
+    //     },
+    //     "& .ps-inner-item": {
+    //       padding: "5px 35px 5px 30px !important",
+    //     },
+    //     "& .ps-inner-item:hover": {
+    //       color: "#868dfb !important",
+    //     },
+    //   }}
+    // >
+      <Sidebar collapsed={isCollapsed}
+        rootStyles={{
+          [`.${sidebarClasses.container}`]: {
+            background: `${colors.primary[400]} !important`,
+            backgroundColor: 'transparent',
+          },
+          [`.${sidebarClasses.root}`]: {
+            border: '0px !important'
+          },
+          [`.${menuClasses.button}:hover`]: {
+            color: '#868dfb',
+            backgroundColor: `${colors.primary[400]}`
+          }
+        }}
+
+      >
+        <Menu>
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h4" color={colors.grey[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+          {!isCollapsed && (
+            <Box mb="25px">
+                <Box display="flex" justifyContent="center" alignItems="center">
+                    <img alt="" width="100px" height="100px" style={{ cursor: 'pointer', borderRadius: '50%'}}/>
+                </Box>
+            </Box>
+          )}
+        </Menu>
+      </Sidebar>
+  );
+};
+
+export default SideBar;
