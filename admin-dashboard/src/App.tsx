d5a0d47bf39436150abe0f2ed678addfbef069c5
duongbx288 <duongbx183901@gmail.com>
2023-05-28 14:43:59 +0700

    feat: re-route pages

--- admin-dashboard/src/App.tsx
+++ admin-dashboard/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { ReactElement, ReactNode, useState } from "react";
 import { ColorModeContext, useMode } from "./assets/theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Dashboard from "./pages/dashboard";
 import Topbar from "./pages/global/Topbar";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import Contacts from "./pages/contacts";
 import FAQuestions from "./pages/faq";
 import Bar from "./pages/bar";
@@ -15,36 +15,46 @@ import Calendar from "./pages/calendar";
 import Pie from "./pages/pie";
 import SideBar from "./pages/global/Sidebar";
 import Form from "./pages/form";
+import Login from "./pages/login/Login";
 
-function App() {
+const ProtectedRoute = () => {
   const [theme, colorMode] = useMode();
-
-  return (
+  const currentUser = true;
+  return currentUser ? (
     <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          <SideBar />
-          <main className="content">
-            <Topbar />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQuestions />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} />
-            </Routes>
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        <SideBar />
+        <main className="content">
+          <Topbar />
+          <Outlet />
+        </main>
+      </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>
+  ) : <Navigate to="/login"/>
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<ProtectedRoute />}>
+        <Route path="" element={<Dashboard />} />
+        <Route path="team" element={<Team />} />
+        <Route path="contacts" element={<Contacts />} />
+        <Route path="invoices" element={<Invoices />} />
+        <Route path="form" element={<Form />} />
+        <Route path="bar" element={<Bar />} />
+        <Route path="pie" element={<Pie />} />
+        <Route path="line" element={<Line />} />
+        <Route path="faq" element={<FAQuestions />} />
+        <Route path="geography" element={<Geography />} />
+        <Route path="calendar" element={<Calendar />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
