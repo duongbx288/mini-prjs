57e7413bbef8ad34c0b55c84b813626c55fe0d87
duongbx288 <duongbx183901@gmail.com>
2023-05-29 21:41:38 +0700

    feat: add invoices and contact page

--- admin-dashboard/src/pages/invoices/index.tsx
+++ admin-dashboard/src/pages/invoices/index.tsx
@@ -1,7 +1,92 @@
+import { mockDataInvoices } from "../../data/mockData";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../assets/theme";
+import Header from "../../components/Header";
 const Invoices = () => {
-    return (
-        <></>
-    )
-}
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns: GridColDef[] = [
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 0.5,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => (
+        <Typography color={colors.greenAccent[500]}>
+          ${params.row.cost}
+        </Typography>
+      ),
+    },
+  ];
+
+  return (
+    <Box m="20px" maxWidth={"auto"}>
+      <Header title="INVOICES" subtitle="Lists of Invoice Balances" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          checkboxSelection
+          rows={mockDataInvoices}
+          columns={columns}
+          sx={{ width: "100%" }}
+        />
+      </Box>
+    </Box>
+  );
+};
 
 export default Invoices;
