739d1bf37131523a73ce75642aea978d9c5ddd73
duongbx288 <duongbx183901@gmail.com>
2023-05-29 14:05:28 +0700

    fix: fix custom hook useLocalStorage

--- admin-dashboard/src/hooks/useLocalStorage.ts
+++ admin-dashboard/src/hooks/useLocalStorage.ts
@@ -1,30 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const useLocalStorage = <T>(keyName: string, defaultValue: T | null | (() => T)) => {
-    const [storedValue, setStoredValue] = useState<T|null>(() => {
-        try {
-            const value = window.localStorage.getItem(keyName);
-            if (value && value != null) {
-                return JSON.parse(value);
-            } else {
-                window.localStorage.setItem(keyName, JSON.stringify(defaultValue));
-                return defaultValue;
-            }
-        } catch (err: unknown) {
-            return defaultValue;
-        }
-    })
-
-    const setValue = (newValue: T | null) => {
-        try {
-            window.localStorage.setItem(keyName, JSON.stringify(newValue));
-        } catch (err: unknown) {
-            window.alert(typeof err + `: ${err}`);
-        }
-        setStoredValue(newValue);
+const useLocalStorage = <T>(keyName: string, defaultValue: T | (() => T)) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      if (defaultValue == null) {
+        window.localStorage.removeItem(keyName);
+      }
+      const value = window.localStorage.getItem(keyName);
+      if (value && value != null) {
+        return JSON.parse(value);
+      } else {
+        window.localStorage.setItem(keyName, JSON.stringify(defaultValue));
+        return defaultValue;
+      }
+    } catch (err: unknown) {
+      return defaultValue;
     }
+  });
 
-    return [storedValue, setValue] as const;
-}
+  const setValue = (newValue: T | null) => {
+    try {
+      window.localStorage.setItem(keyName, JSON.stringify(newValue));
+    } catch (err: unknown) {
+      window.alert(typeof err + `: ${err}`);
+    }
+    setStoredValue(newValue);
+  };
+
+  return [storedValue, setValue] as const;
+};
 
 export default useLocalStorage;
